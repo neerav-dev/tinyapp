8d4384d735407628eee6484abe2aa97c83e1c0f0
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-10 16:17:15 -0400

    Added new route for new urls and added body-parser dependencies

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,10 +37,15 @@ app.get("/set", (req, res) => {
   res.render('urls_index', templateVars);
  });
 
+ app.get("/urls/new", (req, res) => {
+   res.render("urls_new");
+ });
+
  app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
  });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
