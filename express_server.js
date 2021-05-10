4feea24df43f5a6d3c55e7656454e1502fad6db3
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-10 18:00:30 -0400

    Implemented redirect short URLs

--- express_server.js
+++ express_server.js
@@ -46,8 +46,13 @@ app.get("/set", (req, res) => {
   res.render('urls_show', templateVars);
  });
 
+ app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+ });
+
  app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  //console.log(req.body);  // Log the POST request body to the console
   //console.log(generateRandomString(6));
 
   const shortURL = generateRandomString(6);
