edf8cfad231d98b1db63f45a84a7e4c6da83c2c8
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-10 18:03:59 -0400

    Code linting done

--- express_server.js
+++ express_server.js
@@ -23,35 +23,35 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
- app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
- });
+});
 
- app.get("/urls/new", (req, res) => {
-   res.render("urls_new");
- });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
- app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
- });
+});
 
- app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
- });
+});
 
- app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
   //console.log(generateRandomString(6));
 
@@ -62,19 +62,19 @@ app.get("/set", (req, res) => {
   urlDatabase[shortURL] = longURL;
 
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  res.redirect(`/urls/${shortURL}`)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const generateRandomString = function (length) {
+const generateRandomString = function(length) {
   let randomString = [];
   
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz';
   for (let i = 0; i < length; i++) {
-    randomString.push(characters.charAt(Math.floor(Math.random() * characters.length)))  
+    randomString.push(characters.charAt(Math.floor(Math.random() * characters.length)));
   }
   return randomString.join('');
-}
+};
