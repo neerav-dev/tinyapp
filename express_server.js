6ac704741e503792e4eb09ed81e843f1b73a2750
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-11 17:57:34 -0400

    implemented logout logic

--- express_server.js
+++ express_server.js
@@ -76,6 +76,14 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+//LOGOUT
+app.post("/logout", (req, res) => {
+  //const {username} = req.body;
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
