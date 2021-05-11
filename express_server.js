cc63dfd3a8a0a8c354e7678c47cd7d656334f583
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-11 16:21:02 -0400

    cookie parser dependency added

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(cookieParser);
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
