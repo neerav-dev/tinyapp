ffdb82e7d62b83b0fa8bcde8a87a538c77075ca5
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-11 17:52:34 -0400

    implemented login logic

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
@@ -15,18 +15,19 @@ const urlDatabase = {
 
 //ROUTES
 app.get("/urls", (req, res) => {
-  const templateVars = {urlList: urlDatabase};
+  const templateVars = {urlList: urlDatabase, username: req.cookies["username"]};
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const {shortURL} = req.params;
   const longURL = urlDatabase[shortURL];
-  const templateVars = {shortURL, longURL};
+  const templateVars = {shortURL, longURL, username: req.cookies["username"]};
 
   res.render('urls_show', templateVars);
 });
@@ -66,6 +67,13 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[shortURL] = longURL;
   
   res.redirect(`/urls/${shortURL}`);
+});
+
+//LOGIN
+app.post("/login", (req, res) => {
+  const {username} = req.body;
+  res.cookie('username', username);
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
