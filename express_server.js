8d611c412b9e3f7ca0947aaa42b701bf3b2d6e18
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-12 13:28:45 -0400

    implemented registration functionality for tinyapp

--- express_server.js
+++ express_server.js
@@ -13,21 +13,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 //ROUTES
 app.get("/urls", (req, res) => {
-  const templateVars = {urlList: urlDatabase, username: req.cookies["username"]};
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {urlList: urlDatabase, user};
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {username: req.cookies["username"]};
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {urlList: urlDatabase, user};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const {shortURL} = req.params;
   const longURL = urlDatabase[shortURL];
-  const templateVars = {shortURL, longURL, username: req.cookies["username"]};
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {shortURL, longURL, user};
 
   res.render('urls_show', templateVars);
 });
@@ -79,10 +95,56 @@ app.post("/login", (req, res) => {
 //LOGOUT
 app.post("/logout", (req, res) => {
   //const {username} = req.body;
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
+//REGISTER
+app.get("/register", (req, res) => {
+  const templateVars = {user: null};
+  res.render("urls_register", templateVars);
+});
+
+const fetchUser = email => {
+  for (const userID in users) {
+    const user = users[userID];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+const createUser = userParams => {
+  const id = generateRandomString(8);
+  const {email, password} = userParams;
+  
+  if (email) {
+    const userExist = fetchUser(email);
+    if (userExist) {
+      return {error: "User Already Exist!", data: null};
+    }
+    if (password) {
+      users[id] = {id, email, password};
+      return {error: null, data: users[id]};
+    } else {
+      return {error: "Password Required!", data: null};
+    }
+  } else {
+    return {error: "Email Required!", data: null};
+  }
+};
+
+app.post("/register", (req, res) => {
+  const result = createUser(req.body);
+  if (result.error) {
+    res.statusCode = 400;
+    res.send(result.error);
+  } else {
+    res.cookie('user_id', result.data.email);
+    res.redirect(`/urls`);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
